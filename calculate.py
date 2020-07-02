fd944a348a001ff690df32216ffac4f9f35408ca
Usama Boumedjout <uboumedj@student.42.fr>
2020-07-03 00:28:57 +0200

    add square root function and solve neg and pos discriminants

--- calculate.py
+++ calculate.py
@@ -10,11 +10,54 @@ def solve_first_degree(reduced_form):
 		if solution == 0:
 			print(0)
 		else:
-			print(solution)
+			print(round(solution, 6))
 	else:
 		display.error_computing()
+
+def search_interval(start, end, num):
+	middle = (start + end) / 2.0
+	square_middle = middle * middle
+	difference = square_middle - num if square_middle > num else num - square_middle
+	if square_middle == num or difference < 0.000001:
+		return middle
+	elif square_middle < num:
+		return search_interval(middle, end, num)
+	elif square_middle > num:
+		return search_interval(start, middle, num)
+
+def square_root(num):
+	if num < 0:
+		print("error: Syntax error while trying to calculate square root: square root of negative number is impossible!")
+		quit()
+	i = 1
+	found = False
+	result = 0
+	while found == False:
+		if i * i == num:
+			result = i
+			found = True
+		elif i * i > num:
+			result = search_interval(i - 1, i, num)
+			found = True
+		i += 1
+	if result == 0:
+		print("error: Syntax error while trying to calculate square root!")
+		quit()
+	return result
 
 def solve_second_degree(reduced_form, discriminant):
 	a,b,c = reduced_form["a"], reduced_form["b"], reduced_form["c"]
 	if discriminant == 0:
 		print(-b / (2 * a))
+	elif discriminant < 0:
+		imaginary_part = round(square_root(-discriminant) / (2 * a), 6)
+		print(-b / (2 * a)),
+		print("- i *"),
+		print(imaginary_part)
+		print(-b / (2 * a)),
+		print("+ i *"),
+		print(imaginary_part)
+	elif discriminant > 0:
+		discriminant_sqrt = square_root(discriminant)
+		print(round((-b + discriminant_sqrt) / (2 * a), 6))
+		print(round((-b - discriminant_sqrt) / (2 * a), 6))
